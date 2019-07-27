067e59cd0de16dcc9bd376c952bee15e9f70c635
Jithin Jude <jithin.jude68@gmail.com>
2019-07-27 23:01:44 +0530

    Initial commit

---
+++ android/app/src/main/java/com/jithinjude/gdg_flutter_study_jam/MainActivity.java
@@ -0,0 +1,13 @@
+package com.jithinjude.gdg_flutter_study_jam;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
